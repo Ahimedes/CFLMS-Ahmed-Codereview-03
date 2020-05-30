03710029be8f5e99d5f1f067a47a5afcd9660b76
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-30 16:45:15 +0200

    Adds JS

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var nameNode=document.getElementById("name");
+var ageNode=document.getElementById("Age");
+var countryNode=document.getElementById("Country");
+var hpNode=document.getElementById("HP");
+var buttonNode=document.getElementById("calculatebutton");
+var resultNode=document.getElementById("result");
+var cost = 0;
+
+
+function calculateInsurance() {
+
+	switch(countryNode) {
+	    case "Austria":
+	        cost = Number(hpNode.value) * 100 / Number(ageNode.value) + 50;
+	        break;
+	    case "Hungary":
+	        cost = Number(hpNode.value) * 120 / Number(ageNode.value) + 100;
+	        break;
+	    case "Greece":
+	        cost = Number(hpNode.value) * 150 / (Number(ageNode.value)+3) + 50;
+	        break;
+	    default:
+	        cost = "Something is not right...";
+	    }
+	
+	resultNode.innerHTML = nameNode + " your inurance costs " + cost + "€"
+}
+buttonNode.addEventListener("click", calculateInsurance, false);
